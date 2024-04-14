4c3ec269ffa7d195bb2b7cf68ec67a8d9ed3bdf2
PRASUN-SITAULA <psitaula209@gmail.com>
2024-04-14 21:55:07 +0545

    Filter data according to the user input

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -3,8 +3,8 @@ export default function JobCard({title, company_name, location, joblink, descrip
         <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-            <p >{company_name}</p>
-            <p>{location}</p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">{company_name}</p>
+            <p className="mb-4 font-normal text-gray-700 dark:text-gray-400">{location}</p>
             <a href={joblink} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,30 +1,40 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import JobCard from "../components/JobCard"
 
 export default function Search(){
 
     const [jobsData, setJobsData] = useState([])
     const [inputData, setInputData] = useState('')
+    const [filteredJobData, setfilteredJobData] = useState()
+    const [isLoading, setisLoading] = useState(false)
+
+    const fliterData = (inputData) => {
+        inputData = inputData.replace(" ", "+")
+        setfilteredJobData(jobsData[inputData])
+    }
 
     const handleInputChange = (event) => {
         setInputData(event.target.value)
     }
+
     const handleSubmit =  async (e) => {
         e.preventDefault()
         // get the scraped data
         try{
+            setisLoading(true)
             const data = await axios.get("http://127.0.0.1:3000/api/v1/jobs")
             setJobsData(data)
+            setisLoading(false)
+            filteredJobData(jobsData)
         }catch(error){
             console.log(error)
         }
         console.log(inputData)
-       
     }
-
     return(
         <div>
-            <form className="max-w-md mx-auto">   
+            <form className="max-w-xl mx-auto">   
                 <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -32,10 +42,14 @@ export default function Search(){
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input value={inputData} type="search" onChange={handleInputChange} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                    <button onClick={handleSubmit} className="text-white absolute end-2.5 bottom-2.5 bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    <input value={inputData} type="search" onChange={handleInputChange} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Software Engineer, Hardware Engineer or UI Designer" required />
+                    <button onClick={handleSubmit} className="text-white absolute end-2.5 bottom-2.5 bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{isLoading? 'Searching': 'Search'}</button>
                 </div>
             </form>
+            <div>
+                <JobCard title="hello" company_name="nepa" location="jhapa" joblink="http" description="surunga"/>
+            </div>
         </div>
+        
     )
 }
