ad985f96f25bfccf18212d0b8244e9f30a5f4167
PRASUN-SITAULA <psitaula209@gmail.com>
2024-04-14 20:52:18 +0545

    Refactoring of the scrape function

--- backend/controller/scrape.js
+++ backend/controller/scrape.js
@@ -10,44 +10,49 @@ const BROWSERLESS_PORT = process.env.BROWSERLESS_PORT
 const browserWSEndpoint = `ws://localhost:${BROWSERLESS_PORT}?token=${TOKEN}`;
 
 const jobTitles = ['Software+Engineer', 'UI+Designer', 'Hardware+Engineer'];
-const searchQuery = 'Software+Engineer'
-const location = ''
 
-exports.getJobs =  async (req, res) => {
-    try{
 
-      const browser = await puppeteer.launch();
   //       // const browser = await puppeteer.connect({
   //       //     browserWSEndpoint: browserWSEndpoint
   //       // })
-      const url = `https://www.indeed.com/jobs?q=Software+Engineer`
-      const page = await browser.newPage()
-      await page.goto(url)
+const scrapeJobs = async (query) => {
+  const browser = await puppeteer.launch();
+  const url = `https://www.indeed.com/jobs?q=${query}`
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.waitForSelector('.job_seen_beacon')
 
-      await page.waitForSelector('.job_seen_beacon')
- 
-      const newJobs = await page.evaluate(() => {
-        const jobCards = Array.from(document.querySelectorAll('.job_seen_beacon'));
-        console.log(jobCards)
-        return jobCards.map(card => {
-            // for each job find titile, location, joblink, publish date, description
-            const title = card.querySelector('.jobTitle a').innerText || 'N/A';
-            const company_name = card.querySelector('.company_location div span').innerText || 'N/A';
-            const location = card.querySelector('.company_location div div').innerText || 'N/A';
-            const joblink = card.querySelector('.jobTitle a').href || 'N/A';
-            // const publishedDate = card.querySelector('.date').innerText || 'N/A';
-            const description = Array.from(card.querySelectorAll('.heading6 ul li')).map(li => li.innerText).join('\n') || 'N/A';
+  const newJobs = await page.evaluate(() => {
+    const jobsData = Array.from(document.querySelectorAll('.job_seen_beacon'));
+    return jobsData.map(job => {
+        const title = job.querySelector('.jobTitle a').innerText || 'N/A';
+        const company_name = job.querySelector('.company_location div span').innerText || 'N/A';
+        const location = job.querySelector('.company_location div div').innerText || 'N/A';
+        const joblink = job.querySelector('.jobTitle a').href || 'N/A';
+        const description = Array.from(job.querySelectorAll('.heading6 ul li')).map(li => li.innerText).join('\n') || 'N/A';
 
-            return { title, company_name, location, joblink, description };
-        });
-    });
-    console.log(newJobs)
-      await browser.close()
-          
-    }catch(error){
-        console.log(error)
-        if (!res.headersSent) {
-            res.status(400).send(error.message)
-          }
+        return { title, company_name, location, joblink, description };
+    })
+  })
+  await browser.close();
+  return newJobs
+}
+        
+exports.getJobs =  async (req, res) => {
+  try {
+    const allJobs = [];
+    for (const title of jobTitles) {
+      const jobs = await scrapeJobs(title);
+      allJobs.push(jobs);
     }
+    // res.status(200).json({
+    //   status: "success",
+    //   data: newJobs
+    // });
+    console.log(allJobs)
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
 }
